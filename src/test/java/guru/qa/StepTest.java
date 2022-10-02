af6514218a5a0d6a429845eb80f97c6a41f7369f
noredcat <anastasiaredcat@gmail.com>
2022-10-02 14:30:58 +0700

    add change java

--- src/test/java/guru/qa/StepTest.java
+++ src/test/java/guru/qa/StepTest.java
@@ -38,17 +38,4 @@ public class StepTest {
         });
     }
 
-
-    @Test
-    public void testAnnotatedStep() {
-        SelenideLogger.addListener("allure",new AllureSelenide());
-        WebSteps steps = new WebSteps();
-        steps.openMainPage();
-        steps.searchForRepository(REPOSITORY);
-        steps.clickOnRepositoryLink(REPOSITORY);
-        steps.openIssuesTab();
-        steps.takeScreenshot();
-
-
-    }
 }
